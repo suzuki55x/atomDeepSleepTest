2dce085a1c3f1a455f2f354d1b3aa5056456c46e
suzuki_ta <suzuki_ta@alco-ex.jp>
2020-08-26 19:27:42 +0900

    update

--- atomDeepSleepTest.ino
+++ atomDeepSleepTest.ino
@@ -13,24 +13,33 @@ void setup() {
 
   if(esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) {// deepSleep後の復帰かどうか
     M5.dis.drawpix(0, ledColor[0]);// R
+    while(1) {
+      M5.update();
+      if(M5.Btn.isReleased()) break;
+      delay(50);// ボタン離さないとまたdeepSleepに入る可能性があるので。
+    }
+    // Ready
+    M5.dis.drawpix(0, ledColor[1]);// G
   } else {
-    M5.dis.drawpix(0, ledColor[3]);// W   
+    M5.dis.drawpix(0, ledColor[3]);// W
   }
   pinMode(GPIO_NUM_39, INPUT_PULLUP);
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_39, LOW);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_39, LOW);// BTN(GPIO39)がLOWで復帰するように設定
 
-  delay(1000);// スリープと復帰でボタン同じなので…
+  delay(50);
 
-  M5.dis.drawpix(0, ledColor[1]);// G   
+  M5.update();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  M5.update();
   if(M5.Btn.wasReleased()) {
+    M5.dis.clear();// Off
+    delay(50);
     esp_deep_sleep_start();
   } else if (M5.Btn.wasPressed()) {
-      M5.dis.drawpix(0, ledColor[2]);// B
+    M5.dis.drawpix(0, ledColor[2]);// B
   }
-  M5.update();
   delay(10);
 }
